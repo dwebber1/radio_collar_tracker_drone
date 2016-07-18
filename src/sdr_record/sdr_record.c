2c41d3869cc545486fbc345d91eca61eaf669962
Daniel Webber <dwebber1@users.noreply.github.com>
2016-07-18 15:08:34 -0700

    add lib to file

--- src/sdr_record/sdr_record.c
+++ src/sdr_record/sdr_record.c
@@ -32,6 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdbool.h>
 #include "queue.h"
 #include <stdint.h>
+#include <airspy.h>
 
 // Global constants
 #define FILE_CAPTURE_DAEMON_SLEEP_PERIOD_MS	50
